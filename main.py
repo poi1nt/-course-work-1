be17db54aebb9c8acc9dea0611f8bb0bee8e1f45
Po1nt <Ziklas_pro@mail.ru>
2023-04-06 22:30:20 +0700

    course work ver2

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
-import time
+import datetime
 from pprint import pprint
-import os
+import json
 
 with open('info/token.txt', 'r') as file_object:
     token = file_object.read().strip()
+
+def register_info(photo_name, size):
+    info = []
+    info.append({'file_name': str(photo_name) + '.jpg', 'size': size})
+    with open('data.txt', 'a') as file:
+        json.dump(info, file, indent=4)
 
 def dounload_photos(id_user, token):
     url = 'https://api.vk.com/method/photos.getAll'
@@ -15,14 +21,22 @@ def dounload_photos(id_user, token):
         'v': '5.131',
         'photo_sizes': '1'
     }
+    list_photo_name = []
     res = requests.get(url, params).json()
     p = res['response']['items']
+    pprint(p)
     for item in p:
         photo_name = item['likes']['count']
+        if photo_name in list_photo_name:
+            date = datetime.date.fromtimestamp(item['date'])
+            photo_name = f'{photo_name} ({date})'
         for photo in item['sizes']:
-            if photo['type'] == 'z':
+            size = photo['type']
+            if size == 'o':
+                list_photo_name.append(photo_name)
+                register_info(photo_name, size)
                 dounload_photo = requests.get(photo['url'], allow_redirects=True)
-                open(f'{photo_name}.png', 'wb').write(dounload_photo.content)
+                open(f'{photo_name}.jpg', 'wb').write(dounload_photo.content)
 
 
 dounload_photos('280602905', token)
